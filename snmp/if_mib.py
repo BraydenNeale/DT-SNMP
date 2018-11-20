c486c2913e17922db518810f75e39409f8920101
Brayden Neale <brayden.neale@gmail.com>
2018-11-20 16:02:53 +1100

    populate lists for each key metric for each interface

--- snmp/if_mib.py
+++ snmp/if_mib.py
@@ -33,7 +33,8 @@ class IFMIB():
 		self._process_result(gen)
 
 	def _process_result(self,gen):
-		interfaces = []
+		#interfaces = []
+		metrics = {}
 		for item in gen:
 		    errorIndication, errorStatus, errorIndex, varBinds = item
 		    
@@ -44,12 +45,18 @@ class IFMIB():
 		                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
 		    else:
 		    	# TODO handle wrapping when counter32/counter64 overflows
-		    	interface = self._calculate_interface_split(varBinds)
-		    	interfaces.append(interface)
+		    	# A switch can have hundreds of interfaces... blows away custom metrics
+		    	#interface = self._calculate_interface_split(varBinds)
+		    	#interfaces.append(interface)
+		    	self._populate_interface_metrics(varBinds, metrics)
 
-		for interface in interfaces:
-			for name,value in interface.items():
-				print('{} - {}'.format(name,value))
+		#for interface in interfaces:
+			#for name,value in interface.items():
+				#print('{} - {}'.format(name,value))
+
+		for key,value in metrics.items():
+			print(key)
+			print(*value, sep=' ')
 
 	def _calculate_interface_split(self,varBinds):
 		interface = {}
@@ -66,7 +73,19 @@ class IFMIB():
 
 		return interface
 
-	def _calculate_interface_averages(self,varBinds):
-		pass
+	def _populate_interface_metrics(self,varBinds, metrics):
+		metrics.setdefault('bandwidth', []).append(varBinds[0][1])
+		metrics.setdefault('incoming_traffic', []).append(varBinds[1][1])
+		metrics.setdefault('outgoing_traffic', []).append(varBinds[2][1])
+		metrics.setdefault('incoming_errors', []).append(varBinds[3][1])
+		metrics.setdefault('outgoing_errors', []).append(varBinds[4][1])
+		metrics.setdefault('incoming_discards', []).append(varBinds[5][1])
+		metrics.setdefault('outgoing_discards', []).append(varBinds[6][1])
+		metrics.setdefault('incoming_ucast', []).append(varBinds[7][1])
+		metrics.setdefault('outgoing_ucast', []).append(varBinds[8][1])
+		metrics.setdefault('incoming_bcast', []).append(varBinds[9][1])
+		metrics.setdefault('outgoing_bcast', []).append(varBinds[10][1])
+		metrics.setdefault('incoming_mcast', []).append(varBinds[11][1])
+		metrics.setdefault('outgoing_mcast', []).append(varBinds[12][1])
 
 
