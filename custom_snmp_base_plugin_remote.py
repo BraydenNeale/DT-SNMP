3f30e8809fe61776ac460be202ac3ce11bc4c82a
Brayden Neale <brayden.neale@gmail.com>
2018-11-20 18:14:01 +1100

    bundle updated classes into main plugin.py file

--- custom_snmp_base_plugin_remote.py
+++ custom_snmp_base_plugin_remote.py
@@ -115,199 +115,208 @@ def _log_inputs(logger, device, authentication):
     for key,value in authentication:
         logger.info('{} - {}'.format(key,value))
 
-# EXTERNAL CLASSES
 class HostResourceMIB():
-  poller = None
+    poller = None
 
-  mib_name = 'HOST-RESOURCES-MIB'
-
-  def __init__(self, device, authentication):
-    self.poller = Poller(device, authentication)
-
-  def poll_metrics(self):
-    cpu = self._poll_cpu()
-    memory, disk = self._poll_storage()
-
-    metrics = {
-      'cpu': cpu,
-      'memory': memory,
-      'disk': disk,
-    }
-
-    return metrics
-
-  def _poll_cpu(self):
-    cpu_metrics = [
-        'hrProcessorLoad',
-    ]
-    oids = [(self.mib_name, metric) for metric in cpu_metrics]
-    gen = self.poller.snmp_connect_bulk(oids)
-    return self._process_cpu(gen)
-
-  def _process_cpu(self,gen):
-    count = 0
-    total = 0
-    for item in gen:
-      errorIndication, errorStatus, errorIndex, varBinds = item
-      if errorIndication:
-          print(errorIndication)
-      elif errorStatus:
-          print('%s at %s' % (errorStatus.prettyPrint(),
-                              errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-      else:
-        # 1 index per iteration
-          for name, value in varBinds:
-            count += 1
-            total += value
-
-    return (total / float(count))
-
-  def _poll_storage(self):
-    storage_metrics = [
-        'hrStorageDescr',
-        'hrStorageSize',
-        'hrStorageUsed',
-    ]
-    oids = [(self.mib_name, metric) for metric in storage_metrics]
-    gen = self.poller.snmp_connect_bulk(oids)
-
-    return self._process_storage(gen)
-
-  def _process_storage(self,gen):
-    storage = []
-    memory_name = 'Physical memory'
-    memory = 0
-    for item in gen:
-      errorIndication, errorStatus, errorIndex, varBinds = item
-      if errorIndication:
-          print(errorIndication)
-      elif errorStatus:
-          print('%s at %s' % (errorStatus.prettyPrint(),
-                              errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-      else:
-        index = {}
-        index['descriptor'] = varBinds[0][1].prettyPrint()
-        size = varBinds[1][1]
-        used = varBinds[2][1]
-        index['utilisation'] = (used / float(size))*100
-
-        if index['descriptor'] == memory_name:
-          memory = index['utilisation']
-          # TODO handle disk splitting
-          break
-        else: 
-          storage.append(index)
-    
-    return memory, storage
-
-class IFMIB():
-  poller = None
-  oids = None
-
-  mib_name = 'IF-MIB'
-  mib_metrics = [
-      'ifInOctets', # Incoming Traffic
-      'ifHCOutOctets', # Outgoing Traffic,
-      'ifInErrors', # Incoming errors
-      'ifOutErrors', # Outgoing errors
-      'ifInDiscards', # Incoming loss rate
-      'ifOutDiscards', # Outgoing loss rate
-      'ifInUcastPkts',
-      'ifOutUcastPkts',
-  ]
-
-  def __init__(self, device, authentication):
-    self.poller = Poller(device, authentication)
-    self.oids = [(self.mib_name, metric) for metric in self.mib_metrics]
-
-  def poll_metrics(self):
-    gen = self.poller.snmp_connect_bulk(self.oids)
-    self._process_result(gen)
-
-  def _process_result(self,gen):
-    for item in gen:
-        errorIndication, errorStatus, errorIndex, varBinds = item
-        
-        if errorIndication:
-            print(errorIndication)
-        elif errorStatus:
-            print('%s at %s' % (errorStatus.prettyPrint(),
-                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-        else:
-            for varBind in varBinds:
-                print(' = '.join([x.prettyPrint() for x in varBind]))
-
-class Poller():
-    device = {}
-    authentication = {}
-    auth_object = None
-
-    auth_protocols = {
-        'md5': usmHMACMD5AuthProtocol,
-        'sha': usmHMACSHAAuthProtocol,
-        'sha224': usmHMAC128SHA224AuthProtocol,
-        'sha256': usmHMAC192SHA256AuthProtocol,
-        'sha384': usmHMAC256SHA384AuthProtocol,
-        'sha512': usmHMAC384SHA512AuthProtocol,
-        'noauth': usmNoAuthProtocol
-    }
-
-    priv_protocols = {
-        'des': usmDESPrivProtocol,
-        '3des': usm3DESEDEPrivProtocol,
-        'aes': usmAesCfb128Protocol,
-        'aes192': usmAesCfb192Protocol,
-        'aes256': usmAesCfb256Protocol,
-        'nopriv': usmNoPrivProtocol
-    }
+    mib_name = 'HOST-RESOURCES-MIB'
 
     def __init__(self, device, authentication):
-        self.authentication = authentication
-        self.device = device
-        self._build_auth_object()
+        self.poller = Poller(device, authentication)
 
-    def snmp_connect(self, oid):
-        gen = getCmd(
-            SnmpEngine(),
-            self.auth_object,
-            UdpTransportTarget((self.device['host'], self.device['port'])),
-            ContextData(),
-            ObjectType(ObjectIdentity(oid)))
-        return next(gen)
+    def poll_metrics(self):
+        cpu = self._poll_cpu()
+        memory, disk = self._poll_storage()
 
-    def snmp_connect_bulk(self, oids):
-        non_repeaters = 0
-        max_repetitions = 25
+        metrics = {
+            'cpu': cpu,
+            'memory': memory,
+            'disk': disk,
+        }
 
-        if (isinstance(oids, str)):
-            oid_object = [ObjectType(ObjectIdentity(oids))]
-        elif (isinstance(oids, tuple)):
-            oid_object = [ObjectType(ObjectIdentity(*oids))]
-        elif(isinstance(oids, list)): # List of Tuple
-            oid_object = [ObjectType(ObjectIdentity(*oid)) for oid in oids]
+        return metrics
 
-        gen = bulkCmd(
-            SnmpEngine(),
-            self.auth_object,
-            UdpTransportTarget((self.device['host'], self.device['port'])),
-            ContextData(),
-            non_repeaters,
-            max_repetitions,             
-            *oid_object,
-            lexicographicMode=False)
+    def _poll_cpu(self):
+        cpu_metrics = [
+            'hrProcessorLoad',
+        ]
+        oids = [(self.mib_name, metric) for metric in cpu_metrics]
+        gen = self.poller.snmp_connect_bulk(oids)
+        return self._process_cpu(gen)
 
-        return gen
+    def _process_cpu(self,gen):
+        count = 0
+        total = 0
+        for item in gen:
+            errorIndication, errorStatus, errorIndex, varBinds = item
+            if errorIndication:
+                print(errorIndication)
+            elif errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                                    errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            else:
+                # 1 index per iteration
+                for name, value in varBinds:
+                    count += 1
+                    total += value
 
-    def _build_auth_object(self):
-        authentication = self.authentication
-        if (authentication['version'] == 3):
-            self.auth_object = UsmUserData(
-                authentication['user'],
-                authentication['auth']['key'],
-                authentication['priv']['key'],
-                self.auth_protocols.get(authentication['auth']['protocol'], None),
-                self.priv_protocols.get(authentication['priv']['protocol'], None))
-        elif(authentication['version'] == 2):
-            self.auth_object = CommunityData(authentication['user'], mpModel=1)
-        elif(authentication['version'] == 1):
-            self.auth_object = CommunityData(authentication['user'], mpModel=0)
+        return (total / float(count))
+
+    def _poll_storage(self):
+        storage_metrics = [
+            'hrStorageDescr',
+            'hrStorageSize',
+            'hrStorageUsed',
+        ]
+        oids = [(self.mib_name, metric) for metric in storage_metrics]
+        gen = self.poller.snmp_connect_bulk(oids)
+
+        return self._process_storage(gen)
+
+    def _process_storage(self,gen):
+        storage = []
+        memory_name = 'Physical memory'
+        memory = 0
+        for item in gen:
+            errorIndication, errorStatus, errorIndex, varBinds = item
+            if errorIndication:
+                print(errorIndication)
+            elif errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                                    errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            else:
+                index = {}
+                index['descriptor'] = varBinds[0][1].prettyPrint()
+                size = varBinds[1][1]
+                used = varBinds[2][1]
+                index['utilisation'] = (used / float(size))*100
+
+                if index['descriptor'] == memory_name:
+                    memory = index['utilisation']
+                    # TODO handle disk splitting
+                    break
+                else:   
+                    storage.append(index)
+        
+        return memory, storage
+
+class IFMIB():
+    poller = None
+    oids = None
+
+    mib_name = 'IF-MIB'
+    mib_metrics = [
+        'ifSpeed', # Bandwidth
+        'ifHCInOctets', # Incoming Traffic
+        'ifHCOutOctets', # Outgoing Traffic,
+        'ifInErrors', # Incoming errors
+        'ifOutErrors', # Outgoing errors
+        'ifInDiscards',
+        'ifOutDiscards',
+        'ifHCInUcastPkts',
+        'ifHCInBroadcastPkts',
+        'ifHCInMulticastPkts',
+        'ifHCOutUcastPkts',
+        'ifHCOutBroadcastPkts',
+        'ifHCOutMulticastPkts',
+    ]
+    # Mib reference
+    # http://cric.grenoble.cnrs.fr/Administrateurs/Outils/MIBS/?oid=1.3.6.1.2.1.31.1.1.1
+    # http://www.net-snmp.org/docs/mibs/interfaces.html
+
+    def __init__(self, device, authentication):
+        self.poller = Poller(device, authentication)
+        self.oids = [(self.mib_name, metric) for metric in self.mib_metrics]
+
+    def poll_metrics(self):
+        gen = self.poller.snmp_connect_bulk(self.oids)
+        return self._process_result(gen)
+
+    def _process_result(self,gen):
+        data = {}
+        for item in gen:
+            errorIndication, errorStatus, errorIndex, varBinds = item
+            
+            if errorIndication:
+                print(errorIndication)
+            elif errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                                    errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            else:
+                # TODO handle wrapping when counter32/counter64 overflows
+                self._populate_interface_metrics(varBinds, data)
+
+        return self._reduce_interface_metrics(data)
+
+    # A switch can have hundreds of interfaces... blows away custom metrics
+    # Usage
+    #interfaces = []
+    #interface = self._calculate_interface_split(varBinds)
+    #interfaces.append(interface)
+    def _calculate_interface_split(self,varBinds):
+        interface = {}
+        interface['bandwidth'] = varBinds[0][1]
+        interface['incoming_traffic'] = varBinds[1][1]
+        interface['outgoing_traffic'] = varBinds[2][1]
+        interface['incoming_errors'] = varBinds[3][1]
+        interface['outgoing_errors'] = varBinds[4][1]
+        interface['incoming_discards'] = varBinds[5][1]
+        interface['outgoing_discards'] = varBinds[6][1]
+        # Unicast + Broadcast + Multicast
+        interface['incoming_packets'] = float(varBinds[6][1]) + float(varBinds[7][1]) + float(varBinds[8][1])
+        interface['outgoing_packets'] = float(varBinds[9][1]) + float(varBinds[10][1]) + float(varBinds[11][1])
+
+        return interface
+
+    def _populate_interface_metrics(self,varBinds, data):
+        data.setdefault('bandwidth', []).append(varBinds[0][1])
+        data.setdefault('incoming_traffic', []).append(varBinds[1][1])
+        data.setdefault('outgoing_traffic', []).append(varBinds[2][1])
+        data.setdefault('incoming_errors', []).append(varBinds[3][1])
+        data.setdefault('outgoing_errors', []).append(varBinds[4][1])
+        data.setdefault('incoming_discards', []).append(varBinds[5][1])
+        data.setdefault('outgoing_discards', []).append(varBinds[6][1])
+        data.setdefault('incoming_ucast', []).append(varBinds[7][1])
+        data.setdefault('outgoing_ucast', []).append(varBinds[8][1])
+        data.setdefault('incoming_bcast', []).append(varBinds[9][1])
+        data.setdefault('outgoing_bcast', []).append(varBinds[10][1])
+        data.setdefault('incoming_mcast', []).append(varBinds[11][1])
+        data.setdefault('outgoing_mcast', []).append(varBinds[12][1])
+
+    def _reduce_interface_metrics(self, data):
+        # Bandwidth is an interface property...
+        # Sum traffic across all interfaces
+        incoming_traffic = sum(data['incoming_traffic'])
+        outgoing_traffic = sum(data['outgoing_traffic'])
+
+        incoming_errors = sum(data['incoming_errors'])
+        outgoing_errors = sum(data['outgoing_errors'])
+        incoming_discards = sum(data['incoming_discards'])
+        outgoing_discards = sum(data['outgoing_discards'])
+        incoming_ucast = sum(data['incoming_ucast'])
+        outgoing_ucast = sum(data['outgoing_ucast'])
+        incoming_bcast = sum(data['incoming_bcast'])
+        outgoing_bcast = sum(data['outgoing_bcast'])
+        incoming_mcast = sum(data['incoming_mcast'])
+        outgoing_mcast = sum(data['outgoing_mcast'])
+
+        incoming_packets = sum([incoming_ucast, incoming_bcast, incoming_mcast])
+        outgoing_packets = sum([outgoing_ucast, outgoing_bcast, outgoing_mcast])
+
+        # Error rate as defined in original script
+        inbound_error_rate = (incoming_errors / incoming_packets)*100
+        outbound_error_rate = (outgoing_errors / outgoing_packets)*100
+
+        # Loss rate as defined in original script
+        inbound_loss_rate = (incoming_discards / incoming_packets)*100
+        outbound_loss_rate = (outgoing_discards / outgoing_packets)*100
+
+        metrics = {
+            'incoming_traffic': incoming_traffic,
+            'outgoing_traffic': outgoing_traffic,
+            'inbound_error_rate': inbound_error_rate,
+            'outbound_error_rate': outbound_error_rate,
+            'inbound_loss_rate': inbound_loss_rate,
+            'outbound_loss_rate': outbound_loss_rate
+        }
+
+        return metrics
